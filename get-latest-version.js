b55f5ebfdea27c2d0afa7256d303ecae958b5e9a
peaceiris <30958501+peaceiris@users.noreply.github.com>
2019-09-18 01:38:30 +0900

    refactor: Get latest version via homebrew

--- get-latest-version.js
+++ get-latest-version.js
@@ -4,16 +4,14 @@ function getLatestVersion() {
   // return new Promise((resolve, reject) => {
   return new Promise(resolve => {
     const xhr = new XMLHttpRequest();
-    const url = "https://api.github.com/repos/gohugoio/hugo/releases/latest";
+    const url = "https://formulae.brew.sh/api/formula/hugo.json";
     xhr.open("GET", url);
     xhr.send();
 
     xhr.onreadystatechange = function() {
       if (xhr.readyState === 4 && xhr.status === 200) {
         const result = JSON.parse(xhr.responseText);
-        const latestURL = result["assets"][0].browser_download_url;
-        const latestVersion = latestURL.match(/(\d+).(\d+).(\d+)/g)[0];
-
+        const latestVersion = result.versions.stable;
         resolve(latestVersion);
         // } else {
         //   reject(`ERROR: got status ${xhr.status}`);

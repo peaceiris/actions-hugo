d46d0aa12e50a085ba63440911f3dc2eaaf5f8ab
Shohei Ueda <30958501+peaceiris@users.noreply.github.com>
2019-09-18 04:11:47 +0900

    Refactor: Get the latest version from Homebrew (#28)
    Refactoring
    
    * feat: Add reject to Promise
    * feat: Add printing inputs
    * feat: Add error handling to getLatestVersion()
    * feat: show version of Hugo, Go, and Git (Close #23)
    * refactor: Get the latest version from Homebrew (Close #26)
    * refactor: enhance error message of promise reject
    
    Enhancement for workflow
    
    * gha: Add deps (test-prod job needs test job)
    * gha: Fix matrix testing
    * gha: comment out Build production step
    * gha: remove Dump step
    
    Update README
    
    * docs: update readme

--- get-latest-version.js
+++ get-latest-version.js
@@ -1,22 +1,18 @@
 const XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 
 function getLatestVersion() {
-  // return new Promise((resolve, reject) => {
-  return new Promise(resolve => {
+  return new Promise((resolve, reject) => {
     const xhr = new XMLHttpRequest();
-    const url = "https://api.github.com/repos/gohugoio/hugo/releases/latest";
+    const url = "https://formulae.brew.sh/api/formula/hugo.json";
     xhr.open("GET", url);
     xhr.send();
-
     xhr.onreadystatechange = function() {
       if (xhr.readyState === 4 && xhr.status === 200) {
         const result = JSON.parse(xhr.responseText);
-        const latestURL = result["assets"][0].browser_download_url;
-        const latestVersion = latestURL.match(/(\d+).(\d+).(\d+)/g)[0];
-
+        const latestVersion = result.versions.stable;
         resolve(latestVersion);
-        // } else {
-        //   reject(`ERROR: got status ${xhr.status}`);
+      } else if (xhr.readyState === 4 && xhr.status !== 200) {
+        reject(`ERROR: got status ${xhr.status} of ${url}`);
       }
     };
   });
--- index.js
+++ index.js
@@ -1,51 +1,54 @@
 const core = require("@actions/core");
 const tc = require("@actions/tool-cache");
 const io = require("@actions/io");
+const exec = require("@actions/exec");
 const getLatestVersion = require("./get-latest-version");
 
 // most @actions toolkit packages have async methods
 async function run() {
   try {
-    getLatestVersion().then(async function(latestVersion) {
-      let hugoVersion = core.getInput("hugo-version");
-      if (!hugoVersion || hugoVersion === "latest") {
-        hugoVersion = latestVersion;
+    getLatestVersion().then(
+      async function(latestVersion) {
+        let hugoVersion = core.getInput("hugo-version");
+        if (!hugoVersion || hugoVersion === "latest") {
+          hugoVersion = latestVersion;
+        }
+        console.log(`Hugo version: ${hugoVersion}`);
+
+        const extended = core.getInput("extended");
+        console.log(`Hugo extended: ${extended}`);
+        let extendedStr = "";
+        if (extended === "true") {
+          extendedStr = "extended_";
+        }
+
+        console.log(`Operating System: ${process.platform}`);
+
+        const hugoName = `hugo_${extendedStr}${hugoVersion}_Linux-64bit`;
+        core.debug(`hugoName: ${hugoName}`);
+
+        const hugoURL = `https://github.com/gohugoio/hugo/releases/download/v${hugoVersion}/${hugoName}.tar.gz`;
+        core.debug(`hugoURL: ${hugoURL}`);
+
+        const hugoPath = `${process.env.HOME}/bin`;
+        await io.mkdirP(hugoPath);
+        core.addPath(hugoPath);
+
+        // Download and extract Hugo binary
+        const hugoTarball = await tc.downloadTool(hugoURL);
+        const hugoExtractedFolder = await tc.extractTar(hugoTarball, "/tmp");
+        core.debug("hugoExtractedFolder:", hugoExtractedFolder);
+        await io.mv(`${hugoExtractedFolder}/hugo`, hugoPath);
+
+        // Show version
+        await exec.exec('hugo version');
+        await exec.exec('go version');
+        await exec.exec('git --version');
+      },
+      function(error) {
+        core.setFailed(error);
       }
-      core.debug(`Hugo version: ${hugoVersion}`);
-
-      let extended = core.getInput("extended");
-      core.debug(`Hugo extended: ${extended}`);
-
-      let extendedStr = "";
-      if (extended === "true") {
-        extendedStr = "extended_";
-      }
-
-      console.log(`Operating System: ${process.platform}`);
-
-      const hugoName = `hugo_${extendedStr}${hugoVersion}_Linux-64bit`;
-      core.debug(`hugoName: ${hugoName}`);
-
-      const hugoURL = `https://github.com/gohugoio/hugo/releases/download/v${hugoVersion}/${hugoName}.tar.gz`;
-      core.debug(`hugoURL: ${hugoURL}`);
-
-      const hugoPath = `${process.env.HOME}/bin`;
-      await io.mkdirP(hugoPath);
-      core.addPath(hugoPath);
-
-      // Download and extract Hugo binary
-      const hugoTarball = await tc.downloadTool(hugoURL);
-      const hugoExtractedFolder = await tc.extractTar(hugoTarball, "/tmp");
-      core.debug("hugoExtractedFolder:", hugoExtractedFolder);
-      await io.mv(`${hugoExtractedFolder}/hugo`, hugoPath);
-      // },
-      // function(error) {
-      //   console.error(error);
-      //   console.log(
-      //     "HINT: GitHub API Rate limiting",
-      //     "https://developer.github.com/v3/#rate-limiting"
-      //   );
-    });
+    );
   } catch (error) {
     core.setFailed(error.message);
   }

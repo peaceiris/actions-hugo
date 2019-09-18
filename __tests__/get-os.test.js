86eb62d73ef1a6c3bad917e040678df17650918b
peaceiris <30958501+peaceiris@users.noreply.github.com>
2019-09-18 09:59:07 +0900

    feat: Add getOS()

---
+++ __tests__/get-os.test.js
@@ -0,0 +1,17 @@
+const getOS = require("../lib/get-os");
+
+describe("getOS", () => {
+  test("test", () => {
+    expect(getOS("linux")).toBe("Linux");
+    expect(getOS("darwin")).toBe("macOS");
+  });
+
+  test("test exception", () => {
+    expect(() => {
+      getOS("win32");
+    }).toThrowError("Windows is not supported");
+    expect(() => {
+      getOS("centos");
+    }).toThrowError("centos is not supported");
+  });
+});

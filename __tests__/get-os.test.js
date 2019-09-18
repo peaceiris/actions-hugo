302385e6711e4ae5212a95a78e85734341c8851a
peaceiris <30958501+peaceiris@users.noreply.github.com>
2019-09-18 10:39:24 +0900

    gha: Add testing for windows-2019

--- __tests__/get-os.test.js
+++ __tests__/get-os.test.js
@@ -4,12 +4,13 @@ describe("getOS", () => {
   test("test", () => {
     expect(getOS("linux")).toBe("Linux");
     expect(getOS("darwin")).toBe("macOS");
+    expect(getOS("win32")).toBe("Windows");
   });
 
   test("test exception", () => {
-    expect(() => {
-      getOS("win32");
-    }).toThrowError("Windows is not supported");
+    // expect(() => {
+    //   getOS("win32");
+    // }).toThrowError("Windows is not supported");
     expect(() => {
       getOS("centos");
     }).toThrowError("centos is not supported");
